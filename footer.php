908fbdad1871eb7cf3ec3ac7812759418579bc92
Jimmy <jimmyk3.14159@gmail.com>
2016-02-25 10:18:30 -0600

    Added contact link to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer>
-	<a class="link-button center" href="#">Back to top</a>
+	<p class="link-button center"><a href="#">Back to top</a></p>
 	<p class="center"><span style="display:inline-block; transform:rotate(180deg) translateY(-2.5px); -webkit-transform:rotate(180deg translateY(-2.5px); -ms-transform: rotate(180deg) translateY(2.5px); -moz-transform: rotate(180deg) translateY(2.5px);">&copy;</span> <?php echo date("Y"); ?></p>
+	<p class="center"><a href="contact.php">Contact</a></p>
 </footer>
