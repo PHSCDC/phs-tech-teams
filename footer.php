2c132837d530b136bc85df6b2fdc448786c3d44e
Jimmy <jimmyk3.14159@gmail.com>
2016-02-25 10:28:22 -0600

    Moved copyleft below contact link in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<p><a class="link-button center" href="#">Back to top</a></p>
+	<p><a class="link-button center" href="contact.php">Contact</a></p>
 	<p class="center"><span style="display:inline-block; transform:rotate(180deg) translateY(-2.5px); -webkit-transform:rotate(180deg) translateY(-2.5px); -ms-transform: rotate(180deg) translateY(-2.5px); -moz-transform: rotate(180deg) translateY(-2.5px);">
 		&copy;</span> <?php echo date("Y"); ?>
 	</p>
-	<p><a class="link-button center" href="contact.php">Contact</a></p>
 </footer>
