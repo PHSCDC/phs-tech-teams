7266faeba3d09d55c0918dc9b35ee9eaedfacbeb
Jimmy <jimmyk3.14159@gmail.com>
2016-02-25 10:19:49 -0600

    Changed contact link in footer into link-button

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-	<p class="link-button center"><a href="#">Back to top</a></p>
+	<p class="center"><a class="link-button" href="#">Back to top</a></p>
 	<p class="center"><span style="display:inline-block; transform:rotate(180deg) translateY(-2.5px); -webkit-transform:rotate(180deg translateY(-2.5px); -ms-transform: rotate(180deg) translateY(2.5px); -moz-transform: rotate(180deg) translateY(2.5px);">&copy;</span> <?php echo date("Y"); ?></p>
-	<p class="center"><a href="contact.php">Contact</a></p>
+	<p class="center"><a class="link-button" href="contact.php">Contact</a></p>
 </footer>
