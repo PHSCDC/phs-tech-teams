39d003807de08c46cdc569a59373861ece8be2ac
Jimmy K <jimmyk3.14159@gmail.com>
2016-03-28 09:55:02 -0500

    Moved team pages to a subfolder

---
+++ team/vrep.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHS Tech Teams</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php include("header.php"); ?>
+<p>tree dimenstional&trade;</p>
+<?php include("footer.php"); ?>
+</body>
+</html>
