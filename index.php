b1c085dc461bc8ed918e6558666485669c4ae18f
root <root@phscyberdefense.org>
2016-03-30 23:14:57 -0500

    Re-added slide show

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 <?php include("header.php"); ?>
-<!-- <noscript>Please enable JavaScript for an enhanced experience.</noscript> 
+<noscript>Please enable JavaScript for an enhanced experience.</noscript>
 <div class="slideshow">
 	<img src="" id="slides"/>
 	<img src="" id="slidefader"/>
@@ -15,14 +15,15 @@
 	<div id="slideshow-right-arrow"><p>&gt;</p></div>
 	<div id="slideshow-label"></div>
 </div>
--->
 <p>Welcome to the Prairie High School Cyber Defense Home Page!</p>
 <p>Here you can view all of the Tech Team Videos as well as learn more about what we have to offer as a club.
 </p>
 <!--
 <h1 class="center">External Links</h1>
-<a class="card third" href="http://www.codecademy.com/"><img src="img/codecademy.png" alt="Codecademy" /></a> -->
+<a class="card third" href="http://www.codecademy.com/"><img src="img/codecademy.png" alt="Codecademy" /></a>
+-->
+
 <?php include("footer.php"); ?>
 </body>
-<!-- <script src="js/slideshow.js"></script> -->
+<script src="js/slideshow.js"></script>
 </html>
