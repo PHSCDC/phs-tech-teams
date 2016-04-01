7c87fe29fe9767d27cb14155934b11f11c682933
Tristin Ohrt-Stagg <trohrt@quarkfoundation.org>
2016-04-01 17:15:09 -0500

    Misc UI fixes, move login message to footer

--- index.php
+++ index.php
@@ -15,12 +15,17 @@
 	<div id="slideshow-right-arrow"><p>&gt;</p></div>
 	<div id="slideshow-label"></div>
 </div>
+
 <p>
-Welcome to the Prairie High School Technology Teams home page!
-
-Though our domain name includes cyber defense, we have expanded to also include our school's robotics. Some of the students who participate in the IT Olympics work with our school's virtual reality team. All three groups make up what we call our Prairie High School Technology Teams.
-
-The purpose of our site is to provide a range of interested learners with instructional and informative videos, and other sources to help them as they delve more and more into the amazing world of technology!  This collective effort, <b>which was built from the ground up</b>, is our Community Service component of the IT Olympics.  Our aim was to create authentic, useful, student-made videos to serve as an open access resource in an attempt to share some of our knowledge to learners all across the globe.</p>
+	Welcome to the Prairie High School Technology Teams home page!
+</p>
+<br>
+<p>
+	Though our domain name includes cyber defense, we have expanded to also include our school's robotics. Some of the students who participate in the IT Olympics work with our school's virtual reality team. All three groups make up what we call our Prairie High School Technology Teams.
+</p>
+<br>
+<p>
+	The purpose of our site is to provide a range of interested learners with instructional and informative videos, and other sources to help them as they delve more and more into the amazing world of technology!  This collective effort, <b>which was built from the ground up</b>, is our Community Service component of the IT Olympics.  Our aim was to create authentic, useful, student-made videos to serve as an open access resource in an attempt to share some of our knowledge to learners all across the globe.</p>
 
 <?php include("footer.php"); ?>
 </body>
